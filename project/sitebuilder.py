0b915cdcf0ad27930854598c23165553d02bbeb8
juzten <juzten@gmail.com>
2015-11-07 03:05:12 -0500

    moving files around

--- project/sitebuilder.py
+++ project/sitebuilder.py
@@ -1,8 +1,23 @@
 import sys
+import os
 
 from flask import Flask, render_template
 from flask_flatpages import FlatPages
 from flask_frozen import Freezer
+
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def parent_dir(path):
+    '''Return the parent of a directory.'''
+    return os.path.abspath(os.path.join(path, os.pardir))
+
+PROJECT_ROOT = parent_dir(APP_DIR)
+# In order to deploy to Github pages, you must build the static files to
+# the project root
+FREEZER_DESTINATION = PROJECT_ROOT
+FREEZER_REMOVE_EXTRA_FILES = False
+FLATPAGES_MARKDOWN_EXTENSIONS = ['codehilite']
+FLATPAGES_ROOT = os.path.join(APP_DIR, 'pages')
 
 DEBUG = True
 FLASTPAGES_AUTO_RELOAD = DEBUG
